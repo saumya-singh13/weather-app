5b0578005df187131c8f447ef69ef0b6e82151d5
Saumya Singh <143325361+saumya-singh13@users.noreply.github.com>
2024-07-03 11:22:32 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '95e2c24778msh66394d359147c76p1d3ff1jsn6793459f8a96',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL= "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "aca3923050861116e0bf29d8d29d7511";
